6e646d819ffec635e4daeb1d144a8949fc1c0174
jangelesrojas <149634295+jangelesrojas@users.noreply.github.com>
2025-11-19 17:55:06 -0500

    Create variables.tf
    This stores input variables

---
+++ infra/variables.tf
@@ -0,0 +1,4 @@
+variable "project_name" { type = string  default = "misconfig-demo" }
+variable "region"       { type = string  default = "us-east-1" }
+variable "instance_type"{ type = string  default = "t3.micro" }
+variable "openai_api_key" { type = string  default = "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" } # optional demo works partially without a key
