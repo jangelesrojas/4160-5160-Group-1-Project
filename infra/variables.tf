7122b2b3b6f2e5218d2464b968bf78aea615289a
jangelesrojas <149634295+jangelesrojas@users.noreply.github.com>
2025-11-19 20:33:27 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,7 @@
-variable "project_name" { type = string  default = "4160-5160-Group-1-Project" }
+variable "project_name" {
+  type    = string
+  default = "4160-5160-Group-1-Project"
+}
 variable "region"       { type = string  default = "us-east-1" }
 variable "instance_type"{ type = string  default = "t3.micro" }
 variable "openai_api_key" { type = string  default = "" } # optional demo works partially without a key
