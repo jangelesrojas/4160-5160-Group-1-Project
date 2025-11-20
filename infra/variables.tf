f5cf18d25440f7fd5c2b75ce70386657654b8406
jangelesrojas <149634295+jangelesrojas@users.noreply.github.com>
2025-11-19 19:54:18 -0500

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,4 +1,4 @@
 variable "project_name" { type = string  default = "4160-5160-Group-1-Project" }
 variable "region"       { type = string  default = "us-east-1" }
 variable "instance_type"{ type = string  default = "t3.micro" }
-variable "openai_api_key" { type = string  default = "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" } # optional demo works partially without a key
+variable "openai_api_key" { type = string  default = "" } # optional demo works partially without a key
